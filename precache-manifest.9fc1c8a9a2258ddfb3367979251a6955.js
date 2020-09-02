ced35cc4c60352febde84326ba9f2b817c7541cd
Darren Lee <dlee@amplience.com>
2020-09-02 10:37:18 +0100

    [ci skip] Updates

---
+++ precache-manifest.9fc1c8a9a2258ddfb3367979251a6955.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef9aabca595bb34041dbd92092f18c4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c25aaa255f042ee0cada",
+    "url": "./static/js/2.dcd2be1c.chunk.js"
+  },
+  {
+    "revision": "80976a714a5adc457a4faad2b109a6ef",
+    "url": "./static/js/2.dcd2be1c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8e1b4286679e0212c532",
+    "url": "./static/js/main.bc777a08.chunk.js"
+  },
+  {
+    "revision": "6e42ef66096bfd0dc928",
+    "url": "./static/js/runtime-main.2823595a.js"
+  }
+]);
